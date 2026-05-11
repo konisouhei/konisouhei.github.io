4723e0e88d9546807bac91398759418aab3260a9
c-net_11 <c-net_11@c-net-11noiMac.local>
2026-05-11 20:05:27 +0900

    Add GitHub Actions workflow and update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ export default defineConfig({
   // ... 他の設定 ...
 
   // ↓ GitHub Pages用の設定を追加
-  site: 'https://konisouhei.github.io', // 必ずあなたのユーザー名に置き換えてください
+  site: 'https://github.com/konisouhei/konisouhei.github.io.git', // 必ずあなたのユーザー名に置き換えてください
   // ↑ ここまで追加
 });
