d09f4011727b6717800bbdb33795718722b7666b
konisouhei <konisouhei@gmail.com>
2026-05-13 11:53:04 +0900

    削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://konisouhei.github.io",
   base: '/konisouhei/', 
-  output: 'static', // 静的サイトとしてビルド
 });
