097483df5b14a209555e9102fffc5cb65eeab5c5
konisouhei <konisouhei@gmail.com>
2026-05-13 16:05:52 +0900

    Tailwind CSS 導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from "astro/config";
- 
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://konisouhei.github.io",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
